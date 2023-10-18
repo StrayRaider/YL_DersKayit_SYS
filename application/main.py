9b9d5302f186b49f795465536869eb2a9faf0edf
StrayRaider <41emrekaya919@gmail.com>
2023-10-18 20:53:55 +0300

    basic window added

--- application/main.py
+++ application/main.py
@@ -1,6 +1,30 @@
 from libs import sqlLib
 import psycopg2
 
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="Youtube Installation")
+        self.main_box = Gtk.HBox()
+        self.add(self.main_box)
+
+
+def quit_app(arg):
+    print("quiting..")
+    Gtk.main_quit()
+    print("All Done")
+
+win = MyWindow()
+win.connect("destroy", quit_app)
+win.show_all()
+Gtk.main()
+
+
+
+
 
 conn = sqlLib.connect();
 cursor = conn.cursor()
